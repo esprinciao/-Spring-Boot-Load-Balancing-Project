f906578da7ea0d813df0aee87b269d84221723ce
Prince P R <princeriyas2@gmail.com>
2024-09-18 10:04:40 +0530

    Add files via upload

---
+++ lbeurekaservice/src/test/java/com/iiht/eurekaservice/LbeurekaserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iiht.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LbeurekaserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
